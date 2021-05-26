73db2de4d27dd3b34e3e3ec658f8aefd4360c1ea
Delaney M. Woodward <1603716406117005@mil>
2021-05-26 12:56:47 -0400

    change to rotary encoder setup

--- rotary_encoder/rotary_encoder.ino
+++ rotary_encoder/rotary_encoder.ino
@@ -1,58 +1,58 @@
 //rotary encoder pins
-const int pin_CLK = 4;
+const int pin_CLK = 6;
 const int pin_DO = 5;
-const int pin_CS = 6; 
+const int pin_CS = 4; 
 
 //encoder signal input values
-int input_clk;
 int input_do;
-int input_cs;
 
 int bitCounter = 9;
 word current_Data = 0;
-volatile int first_clk = 0;
-volatile int collect_data = 0;
+const int other_bits = 6;
 
-void clk_ISR(){
-  if(first_clk){
-    if(bitCounter > -1){
-      input_do = digitalRead(pin_DO);
-      currentData |= (input_do << bitCounter);    
-      bitCounter--;
-    }
+
+void readData(){
+  digitalWrite(pin_CS, HIGH);
+  digitalWrite(pin_CS, LOW);
+  delay(0.0005); //tCLKFE (500 ns)
+  current_Data = 0;
+  while(bitCounter > -1){
+    //data available 375ns after rising edge
+    digitalWrite(pin_CLK, LOW);
+    digitalWrite(pin_CLK, HIGH);
+    delay(0.000375);
+    input_do = digitalRead(pin_DO);
+    Serial.print(input_do);
+    current_Data |= (input_do << bitCounter);    
+    bitCounter--;
   }
-  else{
-    first_clk = 1;
+  Serial.print("\n");
+  //extra clk ticks for status and parity bits
+  for(int i = 0; i < other_bits; i++){
+    digitalWrite(pin_CLK, LOW);
+    digitalWrite(pin_CLK, HIGH);
+    delay(0.000375);
   }
+  bitCounter = 9;
+  digitalWrite(pin_CLK, LOW);
+  digitalWrite(pin_CLK, HIGH);
+  delay(0.0001); //100 ns
+  digitalWrite(pin_CS, HIGH);  //back to default
+  //Serial.println(current_Data);
 }
 
 void setup() {
-  pinMode(pin_CLK, INPUT);
-  pinMode(pin_DO, INPUT);
-  pinMode(pin_CS, INPUT);
+  pinMode(pin_CLK, OUTPUT);
+  pinMode(pin_DO, INPUT_PULLUP);
+  pinMode(pin_CS, OUTPUT);
+  //set default pin states
+  digitalWrite(pin_CLK, HIGH);
+  digitalWrite(pin_CS, LOW);
+
   Serial.begin(9600);
-  //setup interrupt for falling clk signal (1st is ignored)
-  attachInterrupt(digitalPinToInterrupt(pin_CLK), clk_ISR, FALLING);
 }
 
 void loop() {
-  input_cs = digitalRead(pin_CS);
-  if(input_cs == HIGH){  //encoder inactive -> reset globals
-    first_clk = 0;
-    bitCounter = 9;
-    currentData = 0;
-  }
- 
-  /*if(input_cs == LOW){
-    //new angle to be read
-    while(bitCounter < 10){
-      //input_do = digitalRead(pin_DO);
-      input_clk = digitalRead(pin_CLK);
-      if(input_clk == LOW){
-        
-      }
-      bitCounter++;
-    }
-    bitCounter = 0;
-  }*/
+  readData();
+  delay(500); //measure angle data every 500 ms
 }
