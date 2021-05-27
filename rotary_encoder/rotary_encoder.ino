6bba1c407c6e301a1d1533476ba6cfa3214d4b5e
Delaney M. Woodward <1603716406117005@mil>
2021-05-27 10:37:47 -0400

    bluetooth transmission of encoder values

--- rotary_encoder/rotary_encoder.ino
+++ rotary_encoder/rotary_encoder.ino
@@ -1,3 +1,11 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+const int rxPin = 9;
+const int txPin = 8;
+
+//instantiate serial object
+SoftwareSerial BTSerial(rxPin, txPin);
+
 //rotary encoder pins
 const int pin_CLK = 6;
 const int pin_DO = 5;
@@ -26,7 +34,7 @@ void readData(){
     current_Data |= (input_do << bitCounter);    
     bitCounter--;
   }
-  Serial.print("\n");
+  Serial.print(" ");
   //extra clk ticks for status and parity bits
   for(int i = 0; i < other_bits; i++){
     digitalWrite(pin_CLK, LOW);
@@ -38,17 +46,23 @@ void readData(){
   digitalWrite(pin_CLK, HIGH);
   delay(0.0001); //100 ns
   digitalWrite(pin_CS, HIGH);  //back to default
-  //Serial.println(current_Data);
+  Serial.println(current_Data);
+  BTSerial.print(current_Data);
+  BTSerial.print("\n");
 }
 
 void setup() {
   pinMode(pin_CLK, OUTPUT);
   pinMode(pin_DO, INPUT_PULLUP);
   pinMode(pin_CS, OUTPUT);
+  pinMode(rxPin, INPUT);
+  pinMode(rxPin, OUTPUT);
+  BTSerial.begin(9600);
+  while(!BTSerial);  //wait for connection
+  
   //set default pin states
   digitalWrite(pin_CLK, HIGH);
   digitalWrite(pin_CS, LOW);
-
   Serial.begin(9600);
 }
 
