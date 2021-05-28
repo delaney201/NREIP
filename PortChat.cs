7ceae4904a06ad5d9e5b4dfec7d988a72c889520
Delaney M. Woodward <1603716406117005@mil>
2021-05-28 10:51:22 -0400

    fixed read timing issue

--- PortChat.cs
+++ PortChat.cs
@@ -9,27 +9,28 @@ public class SerialCommunication : MonoBehaviour
 {
     SerialPort data_stream = new SerialPort("COM4", 9600);
     string data_str = "";
+    //char[] data_buffer = {};
     //test variable
     public int value = 0;
 
     void Start()
     {
         data_stream.Open();
-        data_stream.ReadTimeout = 1;  //make shorter than encoder delay
+        //  data_stream.ReadTimeout = 3;  //make shorter than encoder delay
     }
     void Update()
     {
         try
         {
-            data_str = data_stream.ReadLine();  //read single encoder measurement            
+            data_str = data_stream.ReadLine();  //read single encoder measurement 
+            //Debug.Log(data_str);
             if (!data_str.Equals(""))
             {
                 value = int.Parse(data_str);  //check if data shows up in Unity
-                                              // Debug.Log("valid data");
             }
             else
             {
-                //  Debug.Log("invalid data");
+                //Debug.Log("invalid data");
             }
         }
         catch (TimeoutException)
