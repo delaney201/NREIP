8215f82d9a2a4ea497426195aa57b6e793427242
Delaney M. Woodward <1603716406117005@mil>
2021-06-08 12:57:45 -0400

    tracker device recognized

--- testProject/Assets/Teleportation.cs
+++ testProject/Assets/Teleportation.cs
@@ -5,57 +5,37 @@ using Valve.VR;
 
 public class Teleportation : MonoBehaviour
 {
-    public Transform teleport1;
-    public Transform teleport2;
-    private bool teleportSpot = false;
-
-    public SteamVR_Action_Boolean my_Action;
-    private SteamVR_Behaviour_Pose my_Pose = null;
 
     void Awake()
     {
-        my_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        uint ind = 0;
+        var err = ETrackedPropertyError.TrackedProp_Success;
+        for (uint i = 0; i < 16; i++)
+        {
+            var result = new System.Text.StringBuilder((int) 64);
+            OpenVR.System.GetStringTrackedDeviceProperty(i, ETrackedDeviceProperty.Prop_RenderModelName_String, result,
+                64, ref err);
+            if (result.ToString().Contains("tracker"))
+            {
+                ind = i;
+                break;
+            }
+        }
+        //set TrackedObject index
+        GetComponent<SteamVR_TrackedObject>().index = (SteamVR_TrackedObject.EIndex) ind;
     }
     void Update()
     {
         if (SerialCommunication.buttonPressed == 1 && SerialCommunication.prevButtonPressed == 0) //btn has just started being pressed
         {
-            if (my_Action.GetStateUp(my_Pose.inputSource))
-            {
-                TeleportPlayer();
-                teleportSpot = !teleportSpot;
-            }
-            
+            TeleportPlayer();
         }
     }
     public static void TeleportPlayer()
     {
         Debug.Log("im teleporting");
-        //Transform cameraRig = SteamVR_Render.Top().origin;
-
-      
-          /*  Debug.Log("teleporty");   
-            Debug.Log("camera rig");
-            Vector3 headPos = SteamVR_Render.Top().head.position;
-            Debug.Log("head pos");
-            Vector3 headPosToGnd = new Vector3(headPos.x, cameraRig.position.y, headPos.z);
-            Vector3 translatedVect;
-            if (spot)
-            {
-                translatedVect = pos1 - headPosToGnd;
-            }
-            else
-            {
-                translatedVect = pos2 - headPosToGnd;
-            }
-
-            cameraRig.position += translatedVect;*/
+        
     }
 
-    private IEnumerator MoveRig(Transform camRig, Vector3 translation)
-    {
-        yield return null;
-    }
-
-    
+  
 }
